885a23fce01f0c9bc3835d431c8a7b413f2fafdf
yzch0116 <yzch0116@gmail.com>
2020-02-03 11:16:17 -0500

    debug

--- follower_pv7.py
+++ follower_pv7.py
@@ -130,6 +130,7 @@ def turn(ccw):
             kit.continuous_servo[1].throttle = 1
             time.sleep(0.9)
         elif address == 1:
+            print("turn1!!")
             kit.continuous_servo[0].throttle = -1
             kit.continuous_servo[1].throttle = -1
             time.sleep(1.0)

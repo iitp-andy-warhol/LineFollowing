dc3f004406d9a20f255f4636f1a93d2c09585ef1
yzch0116 <yzch0116@gmail.com>
2020-02-03 15:03:11 -0500

    debug

--- piAndy_v2.py
+++ piAndy_v2.py
@@ -125,6 +125,10 @@ def follower():
                 kit.continuous_servo[0].throttle = -1
                 kit.continuous_servo[1].throttle = -1
                 time.sleep(0.9)
+            elif address == 3:
+                kit.continuous_servo[0].throttle = 1
+                kit.continuous_servo[1].throttle = 1
+                time.sleep(1.25)
             else:
                 kit.continuous_servo[0].throttle = 1
                 kit.continuous_servo[1].throttle = 1

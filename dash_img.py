40cac65430b931857fbac088f24f614716733749
yzch0116 <yzch0116@gmail.com>
2020-02-04 15:31:56 -0500

    dash

--- dash_img.py
+++ dash_img.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-dash_memory = np.load("./dash_cam/*.npy")
+dash_memory = np.load("./dash_cam/<built-in function time>.npy")
 
 for frame in range(10):
-    dash_memory = dash_memory[frame*240:(frame+1)*240]
-    cv2.imwrite('./dash_cam/dash{}.jpg'.format(10-frame), dash_memory)
+    save = dash_memory[frame*240:(frame+1)*240]
+    cv2.imwrite('./dash_cam/dash{}.jpg'.format(10-frame), save)

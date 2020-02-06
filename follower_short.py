112eae3483430743252ea9483a88077447ac7d8d
yzch0116 <yzch0116@gmail.com>
2020-02-05 19:03:00 -0500

    short

--- follower_short.py
+++ follower_short.py
@@ -543,11 +543,11 @@ def follower():
                         time_block = True
                         short_time = time.time()
                         print('new time: ', short_time)
-                    elif time.time() - short_time < 2.0:
+                    elif time.time() - short_time < 2.1:
                         if ccw:
                             kit.continuous_servo[0].throttle = 0.4
                             kit.continuous_servo[1].throttle = -1
-                    elif time.time() - short_time >= 2.0:
+                    elif time.time() - short_time >= 2.1:
                         Motor_Steer(-0.4, (error * kp) + (ang * ap), stop=True)
                         address = 2
                 elif operating_drive == 0:

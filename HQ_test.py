62b489eafa9649027b02c1a086ed18d13999230c
yzch0116 <yzch0116@gmail.com>
2020-02-06 21:54:42 -0500

    short

--- HQ_test.py
+++ HQ_test.py
@@ -31,7 +31,7 @@ def send_robot_status(client):
         robot_ping = time.time() - raw_status['ping']
 
         if raw_status != current_raw_status:
-            print("Raw status: ", raw_status, time.strftime('%c', time.localtime(time.time())))
+            # print("Raw status: ", raw_status, time.strftime('%c', time.localtime(time.time())))
             current_raw_status = np.copy(raw_status)
 
         direction = raw_status['direction']

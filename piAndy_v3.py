cd4d7ec7c7b528df29a64b96170c60ea6e507678
yzch0116 <yzch0116@gmail.com>
2020-02-11 22:03:30 -0500

    short2

--- piAndy_v3.py
+++ piAndy_v3.py
@@ -692,7 +692,7 @@ def follower():
                         if time.time() - short_time < 2.3:
                             stop_block = True
                             kit.continuous_servo[0].throttle = -1.0
-                            kit.continuous_servo[1].throttle = 0.21
+                            kit.continuous_servo[1].throttle = 0.22
                         elif time.time() - short_time >= 2.2:
                             Motor_Steer(-0.4, (error * kp) + (ang * ap), stop=True)
                             address = 2
